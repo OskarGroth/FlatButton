50e9b9702fe82ab12b9317d88d41d7a63a827fe1
OskarGroth <oskar@cindori.org>
2016-08-04 12:02:04 +0200

    Initial Commit

---
+++ FlatButton/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FlatButton
+//
+//  Created by Oskar Groth on 04/08/16.
+//  Copyright © 2016 Cindori. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
