906e4625688fe6c4977a716aceb116075a8f588b
OskarGroth <oskar@cindori.org>
2016-10-19 22:08:39 +0900

    Carthage support

---
+++ FlatButton/FlatButton.h
@@ -0,0 +1,19 @@
+//
+//  FlatButton.h
+//  FlatButton
+//
+//  Created by Oskar Groth on 2016-10-19.
+//  Copyright © 2016 Cindori. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FlatButton.
+FOUNDATION_EXPORT double FlatButtonVersionNumber;
+
+//! Project version string for FlatButton.
+FOUNDATION_EXPORT const unsigned char FlatButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlatButton/PublicHeader.h>
+
+
